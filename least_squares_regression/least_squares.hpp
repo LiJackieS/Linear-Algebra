5eabe23f877a543dc92aede71d80a61fd6e3f027
Jackie Li <Lijackie@umich.edu>
2024-09-13 20:29:49 -0400

    Create least_squares.hpp

---
+++ least_squares_regression/least_squares.hpp
@@ -0,0 +1,8 @@
+#ifndef LEAST_SQUARES_HPP
+#define LEAST_SQUARES_HPP
+
+#include <vector>
+
+void least_squares(const std::vector<std::vector<double>>& matrixA, const std::vector<std::vector<double>>& matrixB);
+
+#endif
